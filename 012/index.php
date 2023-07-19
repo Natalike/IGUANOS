27c033f3dd90f1aec64f57deefaa00f97ebee19a
Natalija_L <natalija.liske@gmail.com>
2023-07-19 13:01:59 +0300

    const

--- 012/index.php
+++ 012/index.php
@@ -6,6 +6,15 @@
 
 //$text = file_get_contents('012.txt'); // ('C:\xampp\htdocs\php\IGUANOS\012\012.txt') - pilnas kelias
 
-$text = file_get_contents(__DIR__ . `\012.txt`);
+//$text = file_get_contents(__DIR__ . `\012.txt`);
 
-echo "<h1>$text</h1>";
+//echo "<h1>$text</h1>";
+
+//$animal = json_decode
+//
+//    file_get_contents(__DIR__.'\animal.json'),
+ //   1
+//);
+//
+//echo '<prep>';
+//print_r($animals);
---
+++ 012/main.php
@@ -0,0 +1,5 @@
+<?php
+
+echo '<h1>MAIN</h1>';
+
+include __DIR__ . '/inc.php';
