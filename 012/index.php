c8c8bbbb4be96758751931c1f81f98d4489950a1
Natalija_L <natalija.liske@gmail.com>
2023-07-19 13:08:47 +0300

    012

--- 012/index.php
+++ 012/index.php
@@ -1,20 +1,43 @@
 <?php
 
-//file_put_contents('012.txt', 'Hello Big Brother!');
+// file_put_contents(__DIR__ . '\012.txt', 'Hello Big Brother!');
+// $text = file_get_contents(__DIR__ . '\012.txt');
+// echo "<h1>$text</h1>";
 
-//echo __DIR__ . '<br>';
+// $animals = json_decode
+// (
+//     file_get_contents(__DIR__ . '\animals.json'),
+//     1
+// );
 
-//$text = file_get_contents('012.txt'); // ('C:\xampp\htdocs\php\IGUANOS\012\012.txt') - pilnas kelias
 
-//$text = file_get_contents(__DIR__ . `\012.txt`);
 
-//echo "<h1>$text</h1>";
+// foreach ($animals as $animal) {
+//     echo "<b style=\"color:{$animal['color']}\">{$animal['name']} </b>";
+// }
 
-//$animal = json_decode
-//
-//    file_get_contents(__DIR__.'\animal.json'),
- //   1
-//);
-//
-//echo '<prep>';
-//print_r($animals);
+if (!file_exists(__DIR__ . '\animals.txt')) {
+    echo 'File does not exist!';
+    require __DIR__ . '\data.php';
+}
+
+
+$animals = unserialize
+(
+    file_get_contents(__DIR__ . '\animals.txt')
+);
+
+
+
+foreach ($animals as $animal) {
+    echo "<b style=\"color:{$animal['color']}\">{$animal['name']} </b>";
+}
+
+
+$fox = ['name' => 'fox ' . rand(100, 999), 'color' => 'red'];
+
+$animals[] = $fox;
+
+$niceAnimalsSerealized = serialize($animals);
+
+file_put_contents(__DIR__ . '\animals.txt', $niceAnimalsSerealized);
--- 012/main.php
+++ 012/main.php
@@ -1,5 +1,11 @@
 <?php
+define('GATE', 'OK');
 
 echo '<h1>MAIN</h1>';
 
-include __DIR__ . '/inc.php';
+
+require __DIR__ . '/inc.php';
+require __DIR__ . '/inc.php';
+
+
+echo '<h1>MAIN END</h1>';
