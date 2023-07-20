534f3e86c3ca65d76d8aa15f33c328094335620e
Natalija_L <natalija.liske@gmail.com>
2023-07-20 14:36:06 +0300

    014

--- 014/gp.php
+++ 014/gp.php
@@ -11,9 +11,24 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        <button type="submit" name="submit">Submit</button>
+    <h1><?= $_SERVER['REQUEST_METHOD'] ?> PAGE: <?= $_GET['page'] ?? '-' ?></h1>
+    <h2><?= $_GET['kas'] ?? '-' ?></h2>
+    <h2><?= $_GET['kiek'] ?? '-' ?></h2>
+    <h2 style="color:crimson;"><?= $_POST['kas'] ?? '-' ?></h2>
+    <h2 style="color:crimson;"><?= $_POST['kiek'] ?? '-' ?></h2>
+    <form action="?page=102" method="post">
+        KAS: <input type="text" name="kas">
+        KIEK: <input type="text" name="kiek">
+        <button type="submit">Submit kas kiek POST</button>
     </form>
-    <a href="">Link</a>
+    <form action="" method="get">
+        KAS: <input type="text" name="kas">
+        KIEK: <input type="text" name="kiek">
+        <button type="submit">Submit kas kiek</button>
+    </form>
+    <a href="?kas=barsukas&kiek=daug">Link</a>
+    <a href="?page=1">Page 1</a>
+    <a href="?page=2">Page 2</a>
+    <a href="?page=3">Page 3</a>
 </body>
 </html>
