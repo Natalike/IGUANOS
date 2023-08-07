9c37718782b19446c821dea38ab00a9f22d7da0c
Natalija_L <natalija.liske@gmail.com>
2023-08-07 21:44:49 +0300

    1

---
+++ 023/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// use Bebras\Hello;
+// use Bebras\Hello as Hello;
+// use Bebras\Forest\River;
+// use Lina\Garden\Apple;
+
+// require __DIR__ . '/Lina/Apple.php';
+// require __DIR__ . '/Bebras/River.php';
+// require __DIR__ . '/Bebras/Hello.php';
+// require __DIR__ . '/Lina/Hello.php';
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+$bebras = new Bebras\Hello;
+$lina = new Lina\Hello;
+// $river = new River;
+// $apple = new Apple;
