1300f1402e1b6e0155f0f86de349806fe13e0cbb
Natalija_L <natalija.liske@gmail.com>
2023-08-07 21:48:54 +0300

    change

---
+++ donuts/app/App.php
@@ -0,0 +1,10 @@
+<?php
+namespace Donuts;
+
+class App {
+
+    public static function start() {
+        echo '<h1>Hi from App</h1>';
+    }
+
+}
---
+++ donuts/public/index.php
@@ -0,0 +1,9 @@
+<?php
+use Donuts\App;
+
+require_once '../vendor/autoload.php';
+
+echo '<h1>Hi from index.php</h1>';
+
+
+App::start();
